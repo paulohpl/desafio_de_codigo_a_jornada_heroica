f3a2dab3f12a1e3e6185070ca0dea7693405b6e7
Paulo <153537132+paulohpl@users.noreply.github.com>
2024-02-02 15:41:43 -0300

    desafio javascrip

---
+++ index.js
@@ -0,0 +1,27 @@
+ // Descrição:
+// Você é um jovem herói que embarca em uma jornada épica para derrotar o temido dragão que aterroriza o reino. 
+// No entanto, você precisa atravessar uma floresta perigosa para chegar à caverna do dragão. 
+// Cada passo é crucial, e sua jornada será determinada pela lógica afiada que você possuir.
+
+ // Tarefa: Escreva um algoritmo que simule a jornada do herói pela floresta. 
+// O herói começa em uma posição inicial e deve dar uma série de passos para atravessar a floresta até a caverna do dragão.
+
+ // Entrada:
+ // A posição inicial do herói na floresta (um número inteiro).
+
+ // O número total de passos que o herói deve dar (um número inteiro).
+
+ // Saída:
+ // Imprima a posição final do herói após dar a quantidade de passos especificada.
+ 
+ 
+ const posicaoInicial = 10;
+ const totalPassos = 15;
+
+ function somaPassos () {
+    return posicaoInicial + totalPassos;
+ }
+
+ const novaPosicao = somaPassos();
+
+ console.log(`A nova posição é ${novaPosicao}`);
